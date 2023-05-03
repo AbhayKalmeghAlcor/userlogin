ca32de182dfe360f6731733b3ca59fe3d7d6d147
abhaykalmegh <abhayrameshwar.kalmegh@alcortech.com>
2023-05-03 23:08:30 +0530

    password update

--- logindashboard/logindashboard/settings.py
+++ logindashboard/logindashboard/settings.py
@@ -202,5 +202,5 @@ MEDIA_ROOT = BASE_DIR/'media'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'email'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_USER = 'pictorobo@gmail.com'
+EMAIL_HOST_PASSWORD = 'hubfnpgfrlisfngr'
